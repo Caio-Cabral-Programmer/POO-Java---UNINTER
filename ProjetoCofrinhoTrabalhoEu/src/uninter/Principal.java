4f5939dd13b0fbb8d1c1a31d92667d560e67c27e
Caio Cabral <caiocabral.ep@gmail.com>
2025-03-07 22:41:53 -0300

    Trabalho revisado e enviado

--- ProjetoCofrinhoTrabalhoEu/src/uninter/Principal.java
+++ ProjetoCofrinhoTrabalhoEu/src/uninter/Principal.java
@@ -7,6 +7,9 @@ public class Principal {
     // É criada uma variável menu do tipo Menu, na qual contém o objeto da Classe Menu.
     // Depois é chamado o method executar da classe Menu, que inicia o menu principal do programa.
     public static void main(String[] args) {
+        System.out.println("================================================");
+        System.out.println("TRABALHO COFRINHO || CAIO FELLIPE BEZERRA CABRAL");
+        System.out.println("================================================");
         Menu menu = new Menu();
         menu.executar();
     }
